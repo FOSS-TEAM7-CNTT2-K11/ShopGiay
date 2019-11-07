f2a0a818c1c78e20349d0d424413f78d0d81ca92
giangdv97 <giangokboy@icloud.com>
2019-11-07 07:45:00 +0700

    update entity

--- src/main/java/nhom7/shopgiay/entity/Rate.java
+++ src/main/java/nhom7/shopgiay/entity/Rate.java
@@ -1,10 +1,21 @@
 package nhom7.shopgiay.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "rate")
-public class Rate {
+public class Rate implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
 }
